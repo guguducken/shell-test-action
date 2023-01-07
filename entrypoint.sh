7b4ad4fd1296f0abf65cdba1126745e2cd18676b
guguducken <1173781832@qq.com>
2023-01-07 18:40:34 +0800

    bug fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,7 @@ echo $GITHUB_ENV
 echo $GITHUB_REPOSITORY
 echo $GITHUB_REPOSITORY_OWNER
 echo $GITHUB_SERVER_URL
+echo $GITHUB_API_URL
+echo $GITHUB_REF
 time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
